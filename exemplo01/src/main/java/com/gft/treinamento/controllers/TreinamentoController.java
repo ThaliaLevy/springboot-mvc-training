53f4c5e611f690463d0b0c9f54700e92bc41864a
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-10-07 11:23:33 -0300

    Novas anotações

--- exemplo01/src/main/java/com/gft/treinamento/controllers/TreinamentoController.java
+++ exemplo01/src/main/java/com/gft/treinamento/controllers/TreinamentoController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.gft.treinamento.services.PersonService;
-
 @Controller
 public class TreinamentoController {
 
--- exemplo01/src/main/java/com/gft/treinamento/entities/Person.java
+++ exemplo01/src/main/java/com/gft/treinamento/entities/Person.java
@@ -1,6 +1,15 @@
 package com.gft.treinamento.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private int age;
 	private boolean status;
@@ -22,5 +31,11 @@ public class Person {
 	}
 	public void setStatus(boolean status) {
 		this.status = status;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
